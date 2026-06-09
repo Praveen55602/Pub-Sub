9ad22c5f1a1c7cad4c51cbcdb376322adf73b669
Praveen <praveensingh55602@gmail.com>
2026-06-09 18:00:10 +0530

    v0.1.0: add support for multiple topics

--- main.go
+++ main.go
@@ -8,67 +8,92 @@ import (
 
 type broker struct {
 	mu        sync.Mutex
-	listeners []chan int
+	listeners map[string][]chan int
 }
 
-func (b *broker) addListener() chan int {
+func NewBroker() *broker {
+	return &broker{
+		listeners: make(map[string][]chan int),
+	}
+}
+
+func (b *broker) subscribe(topic string) chan int {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
 	newListener := make(chan int)
-	b.listeners = append(b.listeners, newListener)
+	b.listeners[topic] = append(b.listeners[topic], newListener)
 
 	return newListener
 }
 
-func (b *broker) publish(data int) {
+func (b *broker) publish(topic string, data int) error {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
-	for _, ch := range b.listeners {
+	subscribers, ok := b.listeners[topic]
+	if !ok {
+		return fmt.Errorf("no listeners for the topic %s", topic)
+	}
+
+	for _, ch := range subscribers {
 		ch <- data
 	}
+
+	return nil
 }
 
 func main() {
 	wg := sync.WaitGroup{}
-	b := broker{}
+	b := NewBroker()
 
-	wg.Add(2)
+	topicX := "topic-X"
+	topicY := "topic-Y"
+
+	wg.Add(3)
 
 	go func() {
 		fmt.Println("this is service A")
 
-		q := b.addListener()
+		q := b.subscribe(topicX)
 		wg.Done()
 
 		for data := range q {
-			fmt.Println("service A received a event from broker - ", data)
+			fmt.Printf("service A received data %d on event for topic %s\n", data, topicX)
 		}
 	}()
 
 	go func() {
 		fmt.Println("this is service B")
 
-		q := b.addListener()
+		q := b.subscribe(topicX)
 		wg.Done()
 
 		for data := range q {
-			fmt.Println("service B received a event from broker - ", data)
+			fmt.Printf("service B received data %d on event for topic %s\n", data, topicX)
 		}
 	}()
 
 	go func() {
 		//service C is not subscribing for the event
 		fmt.Println("this is service C")
+
+		q := b.subscribe(topicY)
+		wg.Done()
+
+		for data := range q {
+			fmt.Printf("service C received data %d on event for topic %s\n", data, topicY)
+		}
 	}()
 
 	wg.Wait()
 
 	//some data that needs publishing so that all services subscribed to it receives that data.
-	data := 5
+	data1 := 5
+	data2 := 6
 
-	b.publish(data)
+	b.publish(topicX, data1)
+	b.publish(topicY, data2)
 
 	time.Sleep(2 * time.Second)
 }
