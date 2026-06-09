306a76945b152aec2ed7f75fb12bbfeeefda03ef
Praveen <praveensingh55602@gmail.com>
2026-06-09 14:32:36 +0530

    v0: fix 1. potential race condition 2. add wait group instead of manually waiting

--- main.go
+++ main.go
@@ -2,25 +2,46 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 type broker struct {
+	mu        sync.Mutex
 	listeners []chan int
 }
 
 func (b *broker) addListener() chan int {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
 	newListener := make(chan int)
 	b.listeners = append(b.listeners, newListener)
+
 	return newListener
 }
 
+func (b *broker) publish(data int) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	for _, ch := range b.listeners {
+		ch <- data
+	}
+}
+
 func main() {
-	broker := broker{}
+	wg := sync.WaitGroup{}
+	b := broker{}
+
+	wg.Add(2)
 
 	go func() {
 		fmt.Println("this is service A")
-		q := broker.addListener()
+
+		q := b.addListener()
+		wg.Done()
+
 		for data := range q {
 			fmt.Println("service A received a event from broker - ", data)
 		}
@@ -28,7 +49,10 @@ func main() {
 
 	go func() {
 		fmt.Println("this is service B")
-		q := broker.addListener()
+
+		q := b.addListener()
+		wg.Done()
+
 		for data := range q {
 			fmt.Println("service B received a event from broker - ", data)
 		}
@@ -39,16 +63,12 @@ func main() {
 		fmt.Println("this is service C")
 	}()
 
-	//let the services intialize their queues on the broker
-	time.Sleep(time.Second * 2)
+	wg.Wait()
 
 	//some data that needs publishing so that all services subscribed to it receives that data.
 	data := 5
 
-	//similating a event send from the broker
-	for _, c := range broker.listeners {
-		c <- data
-	}
+	b.publish(data)
 
 	time.Sleep(2 * time.Second)
 }
